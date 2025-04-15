88bcce1e1164da9891303cf3a9ae16221ec60915
Deshan Ulpange <deshanulpange@gmail.com>
2025-04-15 19:27:44 +0530

    add tailwind 3

--- src/Message.tsx
+++ src/Message.tsx
@@ -2,7 +2,7 @@ function Message(){
     const name = "Deshan";
 
     if(name)
-        return <h1> Hello {name} </h1>;
+        return <h1 className="text-5xl"> Hello {name} </h1>;
     return <h1> Hello World </h1>;
 }   
 
