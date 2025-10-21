32cc015f630a34dbeccacf0115268362beb660b3
Deshan Ulpange <deshanulpange@gmail.com>
2025-10-21 20:13:23 +0530

    view all component

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,6 +4,8 @@ import jobs from '../jobs.json';
 
 const JobListings = () => {
 
+    const recentJobs = jobs.slice(0, 3);
+
     return (
         <section className = "bg-blue-50 px-4 py-10" >
             <div className="container-xl lg:container m-auto">
@@ -13,7 +15,7 @@ const JobListings = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                    {jobs.map((job)=>(
+                    {recentJobs.map((job)=>(
                         <JobListing key={job.id} job={job} />
                     ))}
 
