1fb785f65485a3d56a15477f3f59d389d37cf152
Deshan Ulpange <deshanulpange@gmail.com>
2025-12-12 18:57:21 +0530

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ import {
 import MainLayout from './layouts/MainLayout'; 
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<MainLayout/>}> //parent route setup with main layout
             <Route index element={<HomePage/>} />
             <Route path='/jobs' element={<JobsPage/>} />
+            <Route path='*' element={<NotFoundPage/>} />
         </Route>
     )
 );
