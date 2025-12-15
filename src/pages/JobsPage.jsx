65482b8e8db0b05514e1396839698534fca5d668
Deshan Ulpange <deshanulpange@gmail.com>
2025-12-15 19:02:04 +0530

    All Jobs page with JSON

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import JobListings from '../components/JobListings'
 
 const JobsPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <JobListings />
+        </>
+    )
 }
 
 export default JobsPage
